5f8227b110df10154785b57fb4eca64948fb492f
Truong Bao <baotch1312@gmail.com>
2017-12-25 15:00:12 +0700

    test show list

--- src/controller/AdminIndexController.java
+++ src/controller/AdminIndexController.java
@@ -1,8 +1,18 @@
 package controller;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import entity.Cat;
 
 @Controller
 @RequestMapping("admin/portfolio")
@@ -10,7 +20,25 @@ public class AdminIndexController {
 	
 	
 	 @RequestMapping("/index")
-	 public String index(ModelMap modelMap){
+	 public String index(ModelMap modelMap) throws IOException{
+		 
+		// read config file
+        Reader reader = Resources.getResourceAsReader("SqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+
+        // open session
+        SqlSession session = sqlSessionFactory.openSession();
+
+        // get all cat
+        List<Cat> listCat = session.selectList("CatXML.selectAllCat");
+        for (Cat objCat : listCat) {
+            System.out.println(objCat.toString());
+        } 
+
+        // close session
+        session.close();
+
+        
 		return "admin.portfolio.index";
 	 }
 	 
