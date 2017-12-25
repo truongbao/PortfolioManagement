7a5829e152376dee592e6a8a73f696a2a89191d3
thanhle <alee011096@gmail.com>
2017-12-25 11:18:39 +0700

    testdelete2

--- src/controller/AdminIndexController.java
+++ src/controller/AdminIndexController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("admin/portfolio")
 public class AdminIndexController {
 	
-	//le test
 	
 	 @RequestMapping("/index")
 	 public String index(ModelMap modelMap){
