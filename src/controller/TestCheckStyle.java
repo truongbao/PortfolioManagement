8404f2e0e6e72ae549acf7410986d18da0de781e
Truong Bao <baotch1312@gmail.com>
2017-12-25 23:00:41 +0700

    test class TestCheckStyle

---
+++ src/controller/TestCheckStyle.java
@@ -0,0 +1,63 @@
+package controller;
+
+public class TestCheckStyle {
+
+	public static void main(String[] args) {
+        foo();
+    }
+ 
+    private static void foo() {
+        System.out.println("Bla bla bla...");
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = 40;
+        int e = a + b + c + d;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println("Bla bla bla... again");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println("Bla bla bla... again");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println("Bla bla bla... again");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println("Bla bla bla... again");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println("Bla bla bla... again");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println("Bla bla bla... again");
+    }
+ 
+    @Override
+    public boolean equals(Object o) {
+        if(o instanceof TestCheckStyle) {
+            return true;
+        }
+        return false;
+    }
+	
+	
+}
