7105b351b65d5fbf5496aedc6fac75120c35d373
Truong Bao <baotch1312@gmail.com>
2017-12-28 15:08:15 +0700

    fix controller connect DAO 3

--- src/controller/AdminCreatePortfolioController.java
+++ src/controller/AdminCreatePortfolioController.java
@@ -1,46 +1,36 @@
 package controller;
 
 import java.io.IOException;
-import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.ibatis.io.Resources;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import mappings.GroupSecernMapperDao;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import entity.Group;
 import entity.GroupSecern;
 import entity.GroupSecern_ShowGroup;
 import entity.PortfolioGroup;
-import entity.ServicePortfolioConfiguration;
-import mappings.GroupSecernMapper;
 
 @Controller
 @RequestMapping("admin/portfolio")
 public class AdminCreatePortfolioController {
+	
+	@Autowired
+	GroupSecernMapperDao gsmDao;
 
 	// Show form Create
 	@RequestMapping("/create")
 	public String show_form_create(ModelMap modelMap) throws IOException {
-
-		// read config file
-		Reader reader = Resources.getResourceAsReader("SqlMapConfig.xml");
-		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-
-		// open session
-		SqlSession session = sqlSessionFactory.openSession();
-
-		GroupSecernMapper groupSecernMapper = session.getMapper(GroupSecernMapper.class);
-		List<GroupSecern> listGroupSecern1 = groupSecernMapper.selectAllGroupSecern();
+		
+		List<GroupSecern> listGroupSecern1 = gsmDao.selectAllGroupSecern();
 		System.out.println(listGroupSecern1);
-
+		
 		// get listSPConfiguration
 		List<GroupSecern_ShowGroup> listGroup_ShowGroup = new ArrayList<>();
 		// List<GroupSecern> listGroupSecern =
@@ -58,8 +48,6 @@ public class AdminCreatePortfolioController {
 		// listGroup_ShowGroup.add(each_GroupSecern_ShowGroup);
 		// }
 		modelMap.addAttribute("listGroup_ShowGroup", listGroup_ShowGroup);
-		// close session
-		session.close();
 
 		return "admin.portfolio.create";
 	}
--- src/controller/AdminIndexPortfolioController.java
+++ src/controller/AdminIndexPortfolioController.java
@@ -3,27 +3,28 @@ package controller;
 import java.io.IOException;
 import java.util.List;
 
-import mappings.ServicePortfolioConfigurationMapper;
+import mappings.ServicePortfolioConfigurationMapperDao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import utility.Session;
 import entity.ServicePortfolioConfiguration;
 
 @Controller
 @RequestMapping("admin/portfolio")
 public class AdminIndexPortfolioController {
 	
+	
+	@Autowired
+	ServicePortfolioConfigurationMapperDao spcDao;
 
 	@RequestMapping("/index")
 	public String index(ModelMap modelMap) throws IOException {
 		
-		ServicePortfolioConfigurationMapper configurationMapper = Session.session().getMapper(ServicePortfolioConfigurationMapper.class);
-		
-		List<ServicePortfolioConfiguration> listSPConfiguration = configurationMapper.selectAll();
+		List<ServicePortfolioConfiguration> listSPConfiguration = spcDao.selectAll();
 		modelMap.addAttribute("listSPConfiguration", listSPConfiguration);
 		
 		return "admin.portfolio.index";
@@ -33,9 +34,7 @@ public class AdminIndexPortfolioController {
 	@RequestMapping("/index/{refresh}")
 	public String reload_index(ModelMap modelMap, @PathVariable("refresh") String refresh) throws IOException {
 
-		// get listSPConfiguration
-		ServicePortfolioConfigurationMapper configurationMapper = Session.session().getMapper(ServicePortfolioConfigurationMapper.class);
-		List<ServicePortfolioConfiguration> listSPConfiguration = configurationMapper.selectAll();
+		List<ServicePortfolioConfiguration> listSPConfiguration = spcDao.selectAll();
 		modelMap.addAttribute("listSPConfiguration", listSPConfiguration);
 
 		return "admin.portfolio.index";
